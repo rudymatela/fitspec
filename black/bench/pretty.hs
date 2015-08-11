a61a199b2669307d1357502c1ca4b1ff3b4a97da
Rudy Matela <rudy@matela.com.br>
2015-08-11 16:29:12 +0100

    black: Add Hughes' pretty printing library bench

---
+++ black/bench/pretty.hs
@@ -0,0 +1,33 @@
+import FitSpec
+import FitSpecC
+import Test.Check
+import Text.PrettyPrint
+
+instance Listable Doc where
+  listing = cons1 text  -- TODO: Improve this
+
+propertyMap :: Int -> ((Doc,Doc) -> Doc) -> ((Doc,Doc) -> Doc) -> ((Int,Doc) -> Doc) -> [Bool]
+propertyMap n beside' above' nest'' =
+  [ holds n $ \x y z ->       (x <> y) <> z == x <> (y <> z)
+  , holds n $ \x y z ->       (x $$ y) $$ z == x $$ (y $$ z)
+  , holds n $ \x ->            x <> text "" == x
+  , holds n $ \x k y ->     nest k (x $$ y) == nest k x $$ nest k y 
+  , holds n $ \x k y ->     nest k (x <> y) == nest k x <> y
+  , holds n $ \x k y ->       x <> nest k y == x <> y
+  , holds n $ \x k k' -> nest k (nest k' x) == (nest (k+k') x)
+  , holds n $ \x ->                nest 0 x == x
+  , holds n $ \x y z ->       (x $$ y) <> z == x $$ (y <> z)
+  , holds n $ \s y z -> text s <> ((text "" <> y) $$ z) == (text s <> y) $$ nest (length s) z
+  , holds n $ \s t ->      text s <> text t == text (s ++ t)
+  ]
+  where (<>) = curry beside'
+        ($$) = curry above'
+        nest = curry nest''
+
+main = do reportWith args { limitResults = Just 20 }
+                     1000 (uncurry (<>),uncurry ($$),uncurry (nest)) (pmap 2000)
+          -- 2000m x 4000t x 7laws: 1m5s
+  where pmap = uncurry3 . propertyMap
+
+uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
+uncurry3 f = \(x,y,z) -> f x y z
