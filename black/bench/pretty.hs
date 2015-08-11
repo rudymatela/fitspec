61ce995ec04852d71d2f0d2dedcd7eefda887c04
Rudy Matela <rudy@matela.com.br>
2015-08-11 16:37:05 +0100

    black/bench/pretty: add also QS properties

--- black/bench/pretty.hs
+++ black/bench/pretty.hs
@@ -11,7 +11,7 @@ propertyMap n beside' above' nest'' =
   [ holds n $ \x y z ->       (x <> y) <> z == x <> (y <> z)
   , holds n $ \x y z ->       (x $$ y) $$ z == x $$ (y $$ z)
   , holds n $ \x ->            x <> text "" == x
-  , holds n $ \x k y ->     nest k (x $$ y) == nest k x $$ nest k y 
+  , holds n $ \x k y ->     nest k (x $$ y) == nest k x $$ nest k y
   , holds n $ \x k y ->     nest k (x <> y) == nest k x <> y
   , holds n $ \x k y ->       x <> nest k y == x <> y
   , holds n $ \x k k' -> nest k (nest k' x) == (nest (k+k') x)
@@ -24,10 +24,27 @@ propertyMap n beside' above' nest'' =
         ($$) = curry above'
         nest = curry nest''
 
+propertyMapQS :: Int -> ((Doc,Doc) -> Doc) -> ((Doc,Doc) -> Doc) -> ((Int,Doc) -> Doc) -> [Bool]
+propertyMapQS n beside' above' nest'' =
+  [ holds n $ \x y z ->       (x <> y) <> z == x <> (y <> z)
+  , holds n $ \x y z ->       (x $$ y) $$ z == x $$ (y $$ z)
+  , holds n $ \x y z ->       (x $$ y) <> z == x $$ (y <> z)
+  , holds n $ \x k y ->       x <> nest k y == x <> y
+  , holds n $ \x k y ->     nest k (x <> y) == nest k x <> y
+  , holds n $ \x k y ->     nest k (x $$ y) == nest k x $$ nest k y
+  , holds n $ \x k k' -> nest k (nest k' x) == nest k' (nest k x)
+  ]
+  where (<>) = curry beside'
+        ($$) = curry above'
+        nest = curry nest''
+
 main = do reportWith args { limitResults = Just 20 }
                      1000 (uncurry (<>),uncurry ($$),uncurry (nest)) (pmap 2000)
           -- 2000m x 4000t x 7laws: 1m5s
+          reportWith args { limitResults = Just 20 }
+                     1000 (uncurry (<>),uncurry ($$),uncurry (nest)) (pmapQS 2000)
   where pmap = uncurry3 . propertyMap
+        pmapQS = uncurry3 . propertyMapQS
 
 uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
 uncurry3 f = \(x,y,z) -> f x y z
