b21b80b82fe3e592f1b9a074b57dd81fee88d8c9
Rudy Matela <rudy@matela.com.br>
2015-10-20 16:15:34 +0100

    black/bench/spring: simplify

--- black/bench/spring.hs
+++ black/bench/spring.hs
@@ -2,43 +2,48 @@ import FitSpec
 import Data.List
 import Test.Check
 
+type Add  a = a -> a -> a
+type Prod a = a -> a -> a
+type Ty a = ( Add  a
+            , Prod a )
+
 propertyMap :: (Listable a, Show a, Eq a, Num a)
             => Int
-            -> ((a,a)->a, (a,a)->a) -> [Bool]
-propertyMap n (sum',product') =
-  [ holds n $ \x y   ->        x -+- y  ==  y -+- x
-  , holds n $ \x y z -> x -+- (y -+- z) == (x -+- y) -+- z
-  , holds n $ \x     ->         x -+- 0 == x
-  , holds n $ \x     ->         0 -+- x == x
+            -> Add a
+            -> Prod a
+            -> [Bool]
+propertyMap n (+) (*) =
+  [ holds n $ \x y   ->      x + y  ==  y + x
+  , holds n $ \x y z -> x + (y + z) == (x + y) + z
+  , holds n $ \x     ->       x + 0 == x
+  , holds n $ \x     ->       0 + x == x
 
-  , holds n $ \x y   ->        x -*- y  ==  y -*- x
-  , holds n $ \x y z -> x -*- (y -*- z) == (x -*- y) -*- z
-  , holds n $ \x     ->         x -*- 1 == x
-  , holds n $ \x     ->         1 -*- x == x
+  , holds n $ \x y   ->      x * y  ==  y * x
+  , holds n $ \x y z -> x * (y * z) == (x * y) * z
+  , holds n $ \x     ->       x * 1 == x
+  , holds n $ \x     ->       1 * x == x
 
-  , holds n $ \x y z -> x -*- (y -+- z) == (x -*- y) -+- (x -*- z)
-  , holds n $ \x y z -> (y -+- z) -*- x == (y -*- x) -+- (z -*- x)
+  , holds n $ \x y z -> x * (y + z) == (x * y) + (x * z)
+  , holds n $ \x y z -> (y + z) * x == (y * x) + (z * x)
   ]
-  where (-+-) = curry sum'
-        (-*-) = curry product'
 
 
 sargs :: (Integral a, Show a, Read a)
-      => Bool -> Args ((a,a)->a,(a,a)->a)
+      => Bool -> Args (Ty a)
 sargs useExtra =
   args { limitResults = Nothing
-       , showPropertySets = unwords
+       , showPropertySets = unlines
        , extraMutants =
            if useExtra
-             then [ (uncurry (*),     uncurry (+))
-                  , (\(x,y) -> x+y+1, uncurry (*))
-                  , (uncurry (+),     \(x,y) -> x*y+x*y)
-                  , (\(x,y) -> x+y+x+y, uncurry (*))
-                  , (uncurry (+++),   uncurry (*))
-                  , (uncurry (+),     uncurry (+++))
-                  , (uncurry (+++),   uncurry (+++))
-                  , (uncurry min,     uncurry max)
-                  , (uncurry max,     uncurry min)
+             then [ ((*),             (+))
+                  , (\x y -> x+y+1,   (*))
+                  , ((+),             \x y -> x*y+x*y)
+                  , (\x y -> x+y+x+y, (*))
+                  , ((+++),           (*))
+                  , ((+),             (+++))
+                  , ((+++),           (+++))
+                  , (min,             max)
+                  , (max,             min)
                   -- another good example would be
                   -- || and && defined over integers
                   ]
@@ -50,12 +55,12 @@ main :: IO ()
 main = do putStrLn "### Sum and Product Ring ###"
           putStrLn "Simple finite mutations:"
           reportWith (sargs False) 1000
-                     (uncurry (+) :: (Int,Int)->Int,uncurry (*))
-                     (propertyMap 1000)
+                     ((+),(*) :: Prod Int)
+                     (uncurry $ propertyMap 1000)
           putStrLn "Also manual mutations:"
           reportWith (sargs True) 1000
-                     (uncurry (+) :: (Int,Int)->Int,uncurry (*))
-                     (propertyMap 1000)
+                     ((+),(*) :: Prod Int)
+                     (uncurry $ propertyMap 1000)
 
 
 (+++) :: (Show a, Read a, Integral a) => a -> a -> a
