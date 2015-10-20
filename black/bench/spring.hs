9dfb500b74e732b06bb378b742e2c2ef05802bb3
Rudy Matela <rudy@matela.com.br>
2015-10-20 16:34:05 +0100

    black/bench: add cmdargs to spring

--- black/bench/spring.hs
+++ black/bench/spring.hs
@@ -1,6 +1,8 @@
+{-# Language DeriveDataTypeable #-}
 import FitSpec
 import Data.List
 import Test.Check
+import System.Console.CmdArgs hiding (args)
 
 type Add  a = a -> a -> a
 type Prod a = a -> a -> a
@@ -28,11 +30,16 @@ propertyMap n (+) (*) =
   ]
 
 
+fns :: Integral a => Ty a
+fns = ((+),(*))
+
+
 sargs :: (Integral a, Show a, Read a)
       => Bool -> Args (Ty a)
 sargs useExtra =
   args { limitResults = Nothing
        , showPropertySets = unlines
+       , callNames = [ "(+) x y", "(*) x y" ]
        , extraMutants =
            if useExtra
              then [ ((*),             (+))
@@ -51,17 +58,36 @@ sargs useExtra =
        }
 
 
-main :: IO ()
-main = do putStrLn "### Sum and Product Ring ###"
-          putStrLn "Simple finite mutations:"
-          reportWith (sargs False) 1000
-                     ((+),(*) :: Prod Int)
-                     (uncurry $ propertyMap 1000)
-          putStrLn "Also manual mutations:"
-          reportWith (sargs True) 1000
-                     ((+),(*) :: Prod Int)
-                     (uncurry $ propertyMap 1000)
+data CmdArguments = CmdArguments
+  { nMutants :: Int
+  , nTests :: Int
+  , testType :: String
+--, classify :: Bool
+  , useExtraMutants :: Bool
+  } deriving (Data,Typeable,Show,Eq)
 
+
+arguments = CmdArguments
+  { nTests   = 1000    &= help "number of tests to run"
+  , nMutants = 1000    &= help "number of mutants to generate"
+                       &= name "m"
+  , testType = "int"   &= help "type to use"
+                       &= name "type"
+                       &= name "t"
+                       &= explicit
+--, classify = False   &= help "classify mutants, report extra column with fully evaluated ones (grey-box)"
+  , useExtraMutants = False
+                       &= help "pass extra manual mutants to the algorithm (only works for black-box version)"
+                       &= name "e"
+  }
+
+
+main :: IO ()
+main = do as <- cmdArgs arguments
+          run (testType as) (useExtraMutants as) (nMutants as) (nTests as)
+
+run "int" = run' (fns :: Ty Int)
+run' fs em nm nt = reportWith (sargs em) nm fs (uncurry $ propertyMap nt)
 
 (+++) :: (Show a, Read a, Integral a) => a -> a -> a
 x +++ 0 = x
