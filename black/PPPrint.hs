95fe4cb8f8bb83310f8553aad743e5fae5947590
Rudy Matela <rudy@matela.com.br>
2015-10-23 11:19:33 +0100

    black: improve PPPrint
    * remove unused functions
    * add some documentation

--- black/PPPrint.hs
+++ black/PPPrint.hs
@@ -9,35 +9,23 @@ where
 
 import Data.List (intercalate,transpose)
 
--- | Fits a list to a certain width by appending a certain value
---
--- > fit ' ' 6 "str" == "str   "
---
--- > fit 0 6 [1,2,3] == [1,2,3,0,0,0]
-fit :: a -> Int -> [a] -> [a]
-fit x n xs = xs ++ replicate (n - length xs) x
-
--- | normalize makes all list the same length by adding a value
---
--- > normalize ["asdf","qw","er"] == normalize ["asdf","qw  ","er  "]
-normalize :: a -> [[a]] -> [[a]]
-normalize x xs = map (fit x (maxlen xs)) xs
-  where maxlen = maximum . map length
-
 -- | Appends two Strings side by side, line by line
+--
+-- > beside ["asdf\nqw\n","zxvc\nas"] ==
+-- >  "asdfzxvc\n\
+-- >  \qw  as\n"
 beside :: String -> String -> String
 beside cs ds = unlines $ zipWith (++) (normalize ' ' css) dss
   where [css,dss] = normalize "" [lines cs,lines ds]
-
 
 -- | Show elements of a list as a tuple.  If there are multiple lines in any of
 --   the strings, tuple is printed multiline.
 --
 -- > showTuple ["asdf\nqwer\n","zxvc\nasdf\n"] ==
--- >   "( asdf
--- >      qwer
--- >    , zxvc
--- >      asdf )"
+-- >   "( asdf\n\
+-- >   \  qwer\n\
+-- >   \, zxvc\n\
+-- >   \  asdf )\n"
 --
 -- > showTuple ["asdf","qwer"] == "(asdf,qwer)"
 showTuple :: [String] -> String
@@ -54,32 +42,54 @@ showTuple (s:ss) =
 
 -- TODO: make this prettier (see Mutate.hs's mutant printing)
 -- TODO: remove foldr1 use
+
+-- | Formats a table using a given separator.
+--
+-- > table "  " [ ["asdf", "qwer",     "zxvc\nzxvc"]
+-- >            , ["0",    "1",        "2"]
+-- >            , ["123",  "456\n789", "3"] ] ==
+-- >   "asdf  qwer  zxvc\n\
+-- >   \            zxvc\n\
+-- >   \0     1     2\n\
+-- >   \123   456   3\n\
+-- >   \      789\n\"
 table :: String -> [[String]] -> String
 table s sss = unlines
             . map (intercalate s)
             . transpose
-            . map (padl ' ')
+            . map (normalize ' ')
             . foldr1 (zipWith (++))
-            . map (padl "" . map lines)
-            . padl ""
+            . map (normalize "" . map lines)
+            . normalize ""
             $ sss
 
+-- | Given a separator, format strings in columns
+--
+-- > columns " | " ["asdf", "qw\nzxcv", "as\ndf"] ==
+-- >   "asdf | qw   | as\n\
+-- >   \     | zxcv | df\n"
 columns :: String -> [String] -> String
 columns s ss = unlines
              . map (intercalate s)
              . transpose
-             . map (padl ' ')
-             . padl ""
+             . map (normalize ' ')
+             . normalize ""
              . map lines
              $ ss
 
-pad :: Int -> a -> [a] -> [a]
-pad 0 p xs     = xs
-pad n p []     = replicate n p
-pad n p (x:xs) = x:pad (n-1) p xs
+-- | Fits a list to a certain width by appending a certain value
+--
+-- > fit ' ' 6 "str" == "str   "
+--
+-- > fit 0 6 [1,2,3] == [1,2,3,0,0,0]
+fit :: a -> Int -> [a] -> [a]
+fit x n xs = xs ++ replicate (n - length xs) x
 
-padl :: a -> [[a]] -> [[a]]
-padl p xss = map (pad (maxLength xss) p) xss
+-- | normalize makes all list the same length by adding a value
+--
+-- > normalize ["asdf","qw","er"] == normalize ["asdf","qw  ","er  "]
+normalize :: a -> [[a]] -> [[a]]
+normalize x xs = map (x `fit` maxLength xs) xs
 
 maxLength :: [[a]] -> Int
 maxLength = maximum . (0:) . map length
