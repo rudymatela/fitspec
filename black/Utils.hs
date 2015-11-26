1bb07abff2a5a2a645d87b3cfc05bc1064791175
Rudy Matela <rudy@matela.com.br>
2015-11-26 16:50:39 +0000

    ...

--- black/Utils.hs
+++ black/Utils.hs
@@ -40,6 +40,7 @@ import Data.Ord (comparing)
 import Data.List (groupBy,sortBy)
 import Data.IORef (newIORef, readIORef, writeIORef)
 import Control.Concurrent (forkIO, threadDelay, killThread)
+import Control.Monad (liftM)
 
 -- | Compose composed with compose operator.
 --
@@ -116,8 +117,8 @@ bindArgumentType _ f = f
 -- | Transforms a value into 'Just' that value or 'Nothing' on error.
 anyErrorToNothing :: a -> Maybe a
 anyErrorToNothing x = unsafePerformIO $
-  (Just <$> evaluate x) `catch` \e -> do let _ = e :: SomeException
-                                         return Nothing
+  (Just `liftM` evaluate x) `catch` \e -> do let _ = e :: SomeException
+                                             return Nothing
 
 -- | Transforms a value into 'Just' that value or 'Nothing' on some errors:
 --
@@ -127,7 +128,7 @@ anyErrorToNothing x = unsafePerformIO $
 --   * PatternMatchFail
 errorToNothing :: a -> Maybe a
 errorToNothing x = unsafePerformIO $
-  (Just <$> evaluate x) `catches` map ($ return Nothing)
+  (Just `liftM` evaluate x) `catches` map ($ return Nothing)
                                       [ hf (undefined :: ArithException)
                                       , hf (undefined :: ArrayException)
                                       , hf (undefined :: ErrorCall)
