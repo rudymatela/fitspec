aff4882ba6541fb97b748fe4f1fb87885a96e7c4
Rudy Matela <rudy@matela.com.br>
2016-03-14 13:44:02 +0000

    Fix names: UInt -> Word

--- FitSpec/TestTypes.hs
+++ FitSpec/TestTypes.hs
@@ -18,26 +18,26 @@ instance Mutable Int1  where mutiers = mutiersEq
 instance Mutable Int2  where mutiers = mutiersEq
 instance Mutable Int3  where mutiers = mutiersEq
 instance Mutable Int4  where mutiers = mutiersEq
-instance Mutable UInt1 where mutiers = mutiersEq
-instance Mutable UInt2 where mutiers = mutiersEq
-instance Mutable UInt3 where mutiers = mutiersEq
-instance Mutable UInt4 where mutiers = mutiersEq
+instance Mutable Word1 where mutiers = mutiersEq
+instance Mutable Word2 where mutiers = mutiersEq
+instance Mutable Word3 where mutiers = mutiersEq
+instance Mutable Word4 where mutiers = mutiersEq
 -- -}
 {- Alternative implementation:
 instance Mutable Nat   where mutants = mutantsIntegral
 instance Mutable Int2  where mutants = mutantsIntegral
 instance Mutable Int3  where mutants = mutantsIntegral
 instance Mutable Int4  where mutants = mutantsIntegral
-instance Mutable UInt2 where mutants = mutantsIntegral
-instance Mutable UInt3 where mutants = mutantsIntegral
-instance Mutable UInt4 where mutants = mutantsIntegral
+instance Mutable Word2 where mutants = mutantsIntegral
+instance Mutable Word3 where mutants = mutantsIntegral
+instance Mutable Word4 where mutants = mutantsIntegral
 -- -}
 instance ShowMutable Nat   where mutantS = mutantSEq
 instance ShowMutable Int1  where mutantS = mutantSEq
 instance ShowMutable Int2  where mutantS = mutantSEq
 instance ShowMutable Int3  where mutantS = mutantSEq
 instance ShowMutable Int4  where mutantS = mutantSEq
-instance ShowMutable UInt1 where mutantS = mutantSEq
-instance ShowMutable UInt2 where mutantS = mutantSEq
-instance ShowMutable UInt3 where mutantS = mutantSEq
-instance ShowMutable UInt4 where mutantS = mutantSEq
+instance ShowMutable Word1 where mutantS = mutantSEq
+instance ShowMutable Word2 where mutantS = mutantSEq
+instance ShowMutable Word3 where mutantS = mutantSEq
+instance ShowMutable Word4 where mutantS = mutantSEq
