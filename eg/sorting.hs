e6eb6bfbce599cb1abac0a0d649d002b34a4b730
Rudy Matela <rudy@matela.com.br>
2016-06-10 15:18:58 +0100

    Minimize sorting example

--- eg/sorting.hs
+++ eg/sorting.hs
@@ -1,26 +1,43 @@
+-- Minimal example: mutation testing a sort specification
+--
+-- (this program might take a minute to run depending on your system)
+--
+-- Usage:
+--
+-- $ ghc -O2 sorting.hs
+-- $ ./sorting
+-- Apparent incomplete and non-minimal specification based on
+-- 4000 test cases for each of properties 1, 2, 3, 4 and 5
+-- for each of 4000 mutant variations.
+--
+-- 3 survivors (99% killed), smallest:
+--   \xs -> case xs of
+--            [0,0,1] -> [0,1,1]
+--            _ -> sort xs
+--
+-- apparent minimal property subsets:  {1,2,3} {1,2,4}
+-- conjectures:  {3}    =  {4}     96% killed (weak)
+--               {1,3} ==> {5}     98% killed (weak)
+
 import FitSpec
-import Data.List
+import Data.List (sort)
 
-ordered [] = True
-ordered [x] = True
-ordered (x:y:xs) = x<=y && ordered (y:xs)
-
--- Given the number of tests and a sorting implementation
--- return whether each property holds
-properties :: (Show a, Ord a, Listable a) => ([a] -> [a]) -> [Property]
 properties sort =
   [ property $ \xs   -> ordered (sort xs)
   , property $ \xs   -> length (sort xs) == length xs
-  , property $ \x xs -> elem x xs == elem x (sort xs)
-  , property $ \x xs -> count x xs == count x (sort xs)
+  , property $ \x xs -> elem x (sort xs) == elem x xs
+  , property $ \x xs -> notElem x (sort xs) == notElem x xs
+  , property $ \x xs -> minimum (x:xs) == head (sort (x:xs))
   ]
-  where count x = length . filter (==x)
+  where
+  ordered (x:y:xs) = x <= y && ordered (y:xs)
+  ordered _        = True
 
 main =
   reportWith args { names = ["sort xs"]
-                  , nMutants = 2000
-                  , nTests   = 2000
-                  , timeout  = 30
+                  , nMutants = 4000
+                  , nTests   = 4000
+                  , timeout  = 0
                   }
-             (sort::[UInt2]->[UInt2])
+             (sort::[Word2]->[Word2])
              properties
