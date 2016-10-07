65b2f9d798e4bf8ace03f611bec7f257875a9566
Rudy Matela <rudy@matela.com.br>
2016-10-07 11:16:29 +0100

    bench/haskell-src-exts: use deriveMutableCascading

--- bench/haskell-src-exts.hs
+++ bench/haskell-src-exts.hs
@@ -3,93 +3,14 @@ import Test.FitSpec
 import Language.Haskell.Exts
 import Language.Haskell.Exts.Pretty
 
-deriveMutable ''Activation
-deriveMutable ''Alt
-deriveMutable ''Annotation
-deriveMutable ''Assoc
-deriveMutable ''Asst
-deriveMutable ''BangType
-deriveMutable ''Binds
-deriveMutable ''BooleanFormula
-deriveMutable ''Boxed
-deriveMutable ''Bracket
-deriveMutable ''CallConv
-deriveMutable ''ClassDecl
-deriveMutable ''CName
-deriveMutable ''ConDecl
-deriveMutable ''Context
-deriveMutable ''DataOrNew
-deriveMutable ''Decl
-deriveMutable ''DeclHead
-deriveMutable ''Deriving
-deriveMutable ''EWildcard
-deriveMutable ''Exp
-deriveMutable ''ExportSpec
-deriveMutable ''ExportSpecList
-deriveMutable ''FieldDecl
-deriveMutable ''FieldUpdate
-deriveMutable ''FunDep
-deriveMutable ''GadtDecl
-deriveMutable ''GuardedRhs
-deriveMutable ''ImportDecl
-deriveMutable ''ImportSpec
-deriveMutable ''ImportSpecList
-deriveMutable ''InjectivityInfo
-deriveMutable ''InstDecl
-deriveMutable ''InstHead
-deriveMutable ''InstRule
-deriveMutable ''IPBind
-deriveMutable ''IPName
-deriveMutable ''Kind
-deriveMutable ''Literal
-deriveMutable ''Match
-deriveMutable ''Module
-deriveMutable ''ModuleHead
-deriveMutable ''ModuleName
-deriveMutable ''ModulePragma
-deriveMutable ''Name
-deriveMutable ''Namespace
-deriveMutable ''Op
-deriveMutable ''Overlap
-deriveMutable ''Pat
-deriveMutable ''PatField
-deriveMutable ''PatternSynDirection
-deriveMutable ''Promoted
-deriveMutable ''PXAttr
-deriveMutable ''QName
-deriveMutable ''QOp
-deriveMutable ''QualConDecl
-deriveMutable ''QualStmt
-deriveMutable ''ResultSig
-deriveMutable ''Rhs
-deriveMutable ''Role
-deriveMutable ''RPat
-deriveMutable ''RPatOp
-deriveMutable ''Rule
-deriveMutable ''RuleVar
-deriveMutable ''Safety
-deriveMutable ''Sign
-deriveMutable ''SpecialCon
-deriveMutable ''Splice
-deriveMutable ''Stmt
-deriveMutable ''Tool
-deriveMutable ''Type
-deriveMutable ''TypeEqn
-deriveMutable ''TyVarBind
-deriveMutable ''Unpackedness
-deriveMutable ''WarningText
-deriveMutable ''XAttr
-deriveMutable ''XName
-
 instance Eq a => Eq (ParseResult a) where
   ParseOk x == ParseOk y = x == y
   ParseFailed l1 s1 == ParseFailed l2 s2 = l1 == l2 && s1 == s2
   _ == _ = False
 
-deriveMutable ''SrcLoc
-deriveMutable ''ParseResult
-deriveMutable ''SrcSpanInfo
-deriveMutable ''SrcSpan
+deriveMutableCascading ''ParseResult
+deriveMutableCascading ''Module
+deriveMutableCascading ''SrcSpanInfo
 
 properties :: (String -> ParseResult (Module SrcSpanInfo)
               ,Module SrcSpanInfo -> String)
