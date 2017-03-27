f700bafd55f58b9f173c21c2b40d9226ff32f22d
Rudy Matela <rudy@matela.com.br>
2017-03-27 10:26:35 +0100

    Add copyright notices to test files.

--- tests/test-utils.hs
+++ tests/test-utils.hs
@@ -1,3 +1,5 @@
+-- Copyright (c) 2015-2017 Rudy Matela.
+-- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import System.Exit (exitFailure)
 import Data.List (elemIndices,sortBy)
 
