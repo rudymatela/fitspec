cec62be00d15c4568c2782b855418b486b16c791
Rudy Matela <rudy@matela.com.br>
2018-08-22 23:32:20 -0300

    Update copyright notices

--- src/Test/FitSpec.hs
+++ src/Test/FitSpec.hs
@@ -1,6 +1,6 @@
 -- |
 -- Module      : Test.FitSpec
--- Copyright   : (c) 2015-2017 Rudy Matela
+-- Copyright   : (c) 2015-2018 Rudy Matela
 -- License     : 3-Clause BSD  (see the file LICENSE)
 -- Maintainer  : Rudy Matela <rudy@matela.com.br>
 --
